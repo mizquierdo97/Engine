9a2b8e0c0632269ed1a9928f607dfe7c0aa927f6
mizquierdo97 <mizquierdo97@gmail.com>
2017-09-13 17:18:23 +0200

    Base code added

---
+++ Engine/Color.cpp
@@ -0,0 +1,17 @@
+
+#include "Color.h"
+
+Color Red = Color(1.0f, 0.0f, 0.0f);
+Color Green = Color(0.0f, 1.0f, 0.0f);
+Color Blue = Color(0.0f, 0.0f, 1.0f);
+Color Black = Color(0.0f, 0.0f, 0.0f);
+Color White = Color(1.0f, 1.0f, 1.0f);
+Color Yellow = Color(1.0f, 1.0f, 0.0f);
+Color Final1 = Color(1.f,0.f,0.f,1.f);
+Color Final2 = Color(1.f, 0.5f, 0.f, 1.f);
+Color Final3 = Color(1.f, 1.f, 0.f, 1.f);
+Color Final4 = Color(0.f, 1.f, 0.f, 1.f);
+Color Final5 = Color(0.2f, 1.f, 1.f, 1.f);
+Color Final6 = Color(0.f, 0.f, 1.f, 1.f);
+Color Final7 = Color(0.5f, 0.f, 1.f, 1.f);
+
